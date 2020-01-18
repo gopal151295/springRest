13660f0df9a1459fad784614c59a196888357026
Gopal sharma <gopal151295@gmail.com>
2020-01-18 07:27:37 +0530

    basic IOC

---
+++ src/main/java/com/example/demo/BaseballCoach.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class BaseballCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "30 min yoga";
+    }
+}
---
+++ src/main/java/com/example/demo/CricketCoach.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "2 hour sprint";
+    }
+}
